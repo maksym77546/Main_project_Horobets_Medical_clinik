6833cc9b87753199db9afeaf64e099f3bc9bd2f1
maksym77546 <163732127+maksym77546@users.noreply.github.com>
2026-05-19 23:39:10 +0300

    Commit 9: Виправлення авторизації в адмінці (коректний ключ ['login'])

--- admin/check-new.php
+++ admin/check-new.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['admin'])) {
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== 'admin') {
     header('Location: ../login/index.php');
     exit();
 }
